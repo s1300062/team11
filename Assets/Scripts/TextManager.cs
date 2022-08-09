60cf8accb2ea3d604320f3f88e83258cdbc3a662
ShirokaneTaishi <15617tai@gmail.com>
2022-08-10 08:52:22 +0900

    Story1

---
+++ Assets/Scripts/TextManager.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class  TextManager: MonoBehaviour
+{
+   
+    public Text nameText;
+    public Text talkText;
+
+    public bool playing = false;
+    public float textSpeed = 0.1f;
+
+    void Start(){}
+
+   
+    public bool IsClicked()
+    {
+        if (Input.GetMouseButtonDown(0)) return true;
+        return false;
+    }
+
+    // ナレーション用のテキストを生成する関数
+    public void DrawText ( string text)
+    {
+        nameText.text = "";
+        StartCoroutine("CoDrawText", text);
+    }
+    // 通常会話用のテキストを生成する関数
+    public void DrawText ( string name, string text)
+    {
+        nameText.text = name + "\n「";
+        StartCoroutine("CoDrawText", text + "」");
+    }
+
+    // テキストがヌルヌル出てくるためのコルーチン
+    IEnumerator CoDrawText ( string text )
+    {
+        playing = true;
+        float time = 0;
+        while ( true )
+        {
+            yield return 0;
+            time += Time.deltaTime;
+
+            // クリックされると一気に表示
+            if ( IsClicked() ) break;
+
+            int len = Mathf.FloorToInt ( time / textSpeed);
+            if (len > text.Length) break;
+            talkText.text = text.Substring(0, len);
+        }
+        talkText.text = text;
+        yield return 0;
+        playing = false;
+    }
+}
